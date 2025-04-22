9d4232220cf9231a57196a589cda2b3769f23c81
JVelez000 <jvelezbry@gmail.com>
2025-04-22 18:08:54 -0500

    Add files via upload

---
+++ Ejercicio_9.py
@@ -0,0 +1,12 @@
+print("Escribe un año")
+entrada = input(" ")
+
+if entrada.isnumeric():
+    año = int(entrada)
+
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        print(str(año) + " Es un año bisiesto :)")
+    else:
+        print(str(año) + " No es un año bisiesto :(")
+else:
+    print("Ingresa un numero valido / caracter numerico")
