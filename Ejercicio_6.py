54b04fa01c744b9d2950e5b9743e480e43983f82
JVelez000 <jvelezbry@gmail.com>
2025-04-23 00:47:50 -0500

    Update Ejercicio_6.py

--- Ejercicio_6.py
+++ Ejercicio_6.py
@@ -6,8 +6,8 @@ adivina = int(input("Adivina el número (entre 1 y 10): "))
 if adivina == numero:
   print("¡Correcto! adivinaste el numero")
 elif adivina < numero:
-  print("Muy bajo, intenta con uno mas alto")
+  print("Tu numero es mas bajo, intenta con uno mas alto")
 else:
-  print("Muy alto, intenta con uno mas bajo")
+  print("Tu numero es mas alto, intenta con uno mas bajo")
 
 print(f"(El numero era {numero})")
